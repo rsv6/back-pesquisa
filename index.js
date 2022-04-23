8ea51770c0b8b221fcbbf7cbb03596e37a909098
Rafael S.Vieira <rafaelsvbr@gmail.com>
2022-04-23 19:07:05 -0300

    modify response api

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/pesquisa/acesso', (req, res) => {
-  const { pesquisa } = req.body
-  console.log(pesquisa)
+  const cliente = req.body
+  console.log("Rota: /pesquisa/acesso: ", cliente)
 
-  return res.status(200).json(pesquisa)
+  return res.status(200).json(1)
 })
 
 app.post('/pesquisa/notificacao', (req, res) => {
   const { notificacao } = req.body
-  console.log("Notificacao: ", notificacao)
+  console.log("Rota: /pesquisa/notificacao: ", notificacao)
 
   return res.status(200).json(notificacao)
 })
 
 app.post('/pesquisa/nota', (req, res) => {
-  const { cliente } = req.body
-  console.log(cliente)
+  const cliente = req.body
+  console.log("Rota: /pesquisa/nota: ", cliente)
   
-  return res.status(200).json(1)
+  return res.status(200).json(cliente)
 })
 
 app.listen(8080, console.log('Server running on port 8080'))
